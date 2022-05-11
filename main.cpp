c23304bbcc1f683ad5b41c7b98e20a8548681171
Mon nom <monadressemail@serveur.com>
2022-05-11 10:32:01 +0200

    Fonction saisie en +

--- main.cpp
+++ main.cpp
@@ -1,16 +1,11 @@
 #include <string>
+#include <iostream>
+
 
 
 bool deplacer_personnage(int &x, int &y, std::string cmd);
 
-int main(int argc, char** argv)
-{
-int x=5, y=4;
-deplacer_personnage(x, y, "UP");
-return 0;
-}
-
-bool deplacer_personnage(int x, int y, std::string cmd)
+bool deplacer_personnage(int &x, int &y, std::string cmd)
 {
 const int xmin = 0;
 const int xmax = 32;
@@ -19,23 +14,42 @@ const int ymax = 15;
 
 if (cmd == "RIGHT")
     {
-    x+=1;
+    if(x+=1 < xmax){
+    x++;
     return true;
     }
+    else
+        return false;
+    }
+
 else if (cmd == "LEFT")
     {
-    x-=1;
+    if(x-=1 > xmin){
+    x--;
     return true;
     }
+    else
+        return false;
+    }
+
 else if (cmd == "DOWN")
     {
-    y+=1;
+    if(y+=1 < ymax){
+    y++;
     return true;
     }
+    else
+        return false;
+    }
+
 else if (cmd == "UP")
     {
-    y-=1;
+    if(y-=1 > ymax){
+    y--;
     return true;
+    }
+    else
+        return false;
     }
 else if (cmd == "IDLE")
     {
@@ -43,4 +57,15 @@ else if (cmd == "IDLE")
     }
 else
         return false;
+}
+
+int main(int argc, char** argv)
+{
+
+    std::string saisie;
+    std::cin >> saisie;
+    std::cout << "Vous avez saisi " << saisie << std::endl;
+    int x=5, y=4;
+    deplacer_personnage(x, y, "UP");
+    return 0;
 }
